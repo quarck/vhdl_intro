a0041d3dfceec16876751a03e01ea1b73f99ad35
Sergey Parshin <quarck@gmail.com>
2019-10-07 17:50:32 +0100

    Updated ALU and memory

--- source_code/part2/cpu/alu.vhd
+++ source_code/part2/cpu/alu.vhd
@@ -5,108 +5,101 @@ use ieee.std_logic_unsigned.all ;
 entity ALU is
 	port
 	(
-		operation	: in std_logic_vector (3 downto 0);
-		left_arg	: in std_logic_vector(7 downto 0);
-		right_arg	: in std_logic_vector(7 downto 0);
-		result		: buffer std_logic_vector(7 downto 0);
-		cin			: in std_logic;
-		n,z,c,b,v 	: buffer std_logic		
+		operation			: in std_logic_vector (3 downto 0);
+		left_arg				: in std_logic_vector(7 downto 0);
+		right_arg			: in std_logic_vector(7 downto 0);
+		carry_in				: in std_logic;
+		result				: out std_logic_vector(7 downto 0);
+		negative_flag		: out std_logic;
+		zero_flag 			: out std_logic;
+		carry_out 			: out std_logic;
+		borrow_out 			: out std_logic;
+		overflow_flag 		: out std_logic
+		
 	);
 end ALU;
 
-architecture ula1 of ula is
-constant adic : std_logic_vector(3 downto 0):="0001";
-constant sub  : std_logic_vector(3 downto 0):="0010";
-constant ou   : std_logic_vector(3 downto 0):="0011";
-constant e    : std_logic_vector(3 downto 0):="0100";
-constant nao  : std_logic_vector(3 downto 0):="0101";
-constant dle  : std_logic_vector(3 downto 0):="0110";
-constant dld  : std_logic_vector(3 downto 0):="0111";
-constant dae  : std_logic_vector(3 downto 0):="1000";
-constant dad  : std_logic_vector(3 downto 0):="1001";
 
+architecture behaviour of ALU is
+
+	constant OP_ADD 	: std_logic_vector(3 downto 0):="0001";
+	constant OP_SUB  	: std_logic_vector(3 downto 0):="0010";
+	constant OP_OR   	: std_logic_vector(3 downto 0):="0011";
+	constant OP_AND   	: std_logic_vector(3 downto 0):="0100";
+	constant OP_NOT  	: std_logic_vector(3 downto 0):="0101";
+	constant OP_SHRC  	: std_logic_vector(3 downto 0):="0110";
+	constant OP_SHRZ  	: std_logic_vector(3 downto 0):="0111";
+	constant OP_SHLC  	: std_logic_vector(3 downto 0):="1000";
+	constant OP_SHLZ  	: std_logic_vector(3 downto 0):="1001";
+
+	signal internal_result : std_logic_vector(7 downto 0);
+	
 begin
-	process (left_arg, right_arg, operation,result,cin)
-	variable temp : std_logic_vector(8 downto 0);
+	process (left_arg, right_arg, operation, carry_in)
+		variable temp : std_logic_vector(8 downto 0);
 	begin
 		case operation is
-		when adic =>
-			temp := ('0'&left_arg) + ('0'&right_arg);
-			result <= temp(7 downto 0);
-			c <= temp(8);
-			if (left_arg(7)=right_arg(7)) then
-				if (left_arg(7) /= result(7)) then v <= '1';
-					else v <= '0';
-				end if;
-			else v <= '0';
+		when OP_ADD =>
+			temp := ('0' & left_arg) + ('0' & right_arg); -- not adding carry_in?
+			internal_result <= temp(7 downto 0);
+			carry_out <= temp(8);
+			
+			if left_arg(7)=right_arg(7) then 
+				overflow_flag <= (left_arg(7) xor temp(7));
+			else 
+				overflow_flag <= '0';
 			end if;
-		when sub =>
+			
+		when OP_SUB =>
 			temp := ('0'&left_arg) - ('0'&right_arg);
-			result <= temp(7 downto 0);
-			b <= temp(8);
-			if (left_arg(7) /= right_arg(7)) then
-				if (left_arg(7) /= result(7)) then v <= '1';
-					else v <= '0';
-				end if;
-			else v <= '0';
+			internal_result <= temp(7 downto 0);
+			borrow_out <= temp(8);			
+			
+			if left_arg(7) /= right_arg(7) then 
+				overflow_flag <= (left_arg(7) xor temp(7));
+			else
+				overflow_flag <= '0';
 			end if;
-		when ou =>
-			result <= left_arg or right_arg;
-		when e =>
-			result <= left_arg and right_arg;
-		when nao =>
-			result <= not left_arg;
-		when dle =>
-			c <= left_arg(7);
-			result(7) <= left_arg(6);
-			result(6) <= left_arg(5);
-			result(5) <= left_arg(4);
-			result(4) <= left_arg(3);
-			result(3) <= left_arg(2);
-			result(2) <= left_arg(1);
-			result(1) <= left_arg(0);
-			result(0) <= cin;
-		when dae =>
-			c <= left_arg(7);
-			result(7) <= left_arg(6);
-			result(6) <= left_arg(5);
-			result(5) <= left_arg(4);
-			result(4) <= left_arg(3);
-			result(3) <= left_arg(2);
-			result(2) <= left_arg(1);
-			result(1) <= left_arg(0);
-			result(0) <= '0';
-		when dld =>
-			c <= left_arg(0);
-			result(0) <= left_arg(1);
-			result(1) <= left_arg(2);
-			result(2) <= left_arg(3);
-			result(3) <= left_arg(4);
-			result(4) <= left_arg(5);
-			result(5) <= left_arg(6);
-			result(6) <= left_arg(7);
-			result(7) <= cin;
-		when dad =>
-			c <= left_arg(0);
-			result(0) <= left_arg(1);
-			result(1) <= left_arg(2);
-			result(2) <= left_arg(3);
-			result(3) <= left_arg(4);
-			result(4) <= left_arg(5);
-			result(5) <= left_arg(6);
-			result(6) <= left_arg(7);
-			result(7) <= '0';		
+			
+		when OP_OR =>
+			internal_result <= left_arg or right_arg;
+			
+		when OP_AND =>
+			internal_result <= left_arg and right_arg;
+			
+		when OP_NOT =>
+			internal_result <= not left_arg;
+			
+		when OP_SHRC =>
+			carry_out <= left_arg(7);
+			internal_result(7 downto 1) <= left_arg(6 downto 0);
+			internal_result(0) <= carry_in;
+			
+		when OP_SHRZ =>
+			carry_out <= left_arg(7);
+			internal_result(7 downto 1) <= left_arg(6 downto 0);
+			internal_result(0) <= '0';
+			
+		when OP_SHLC =>
+			carry_out <= left_arg(0);
+			internal_result(6 downto 0) <= left_arg(7 downto 1); 
+			internal_result(7) <= carry_in;
+			
+		when OP_SHLZ =>
+			carry_out <= left_arg(0);
+			internal_result(6 downto 0) <= left_arg(7 downto 1); 
+			internal_result(7) <= '0';		
+			
 		when others =>
-			result <= "00000000";
-			z <= '0';
-			n <= '0';
-			c <= '0';
-			v <= '0';
-			b <= '0';
+			internal_result <= "00000000";
+			carry_out <= '0';
+			overflow_flag <= '0';
+			borrow_out <= '0';
 		end case;
-		if (result="00000000") then 
-			z <= '1'; else z <= '0';
-		end if;
-		n <= result(7);
 	end process;
-end ula1;
+
+	result <= internal_result;
+	zero_flag <= '1' when internal_result="00000000" else '0';		
+	negative_flag <= internal_result(7);
+	
+end behaviour;
--- source_code/part2/cpu/memory.vhd
+++ source_code/part2/cpu/memory.vhd
@@ -2,77 +2,77 @@ library ieee ;
 use ieee.std_logic_1164.all ;
 use ieee.std_logic_unsigned.all ;
 
-entity memoria is
+entity memory is
 	port
 	(
 		address_bus	: in integer range 0 to 255;
 		data_in		: in integer range 0 to 255;
-		data_out	: out integer range 0 to 255;
+		data_out		: out integer range 0 to 255;
 		mem_write	: in std_logic;
 		rst			: in std_logic
 	);
-end memoria;
+end memory;
 
-architecture memo of memoria is
-constant nop : integer := 0;
-constant sta : integer := 16;
-constant lda : integer := 32;
-constant add : integer := 48;
-constant ior : integer := 64;
-constant iand: integer := 80;
-constant inot: integer := 96;
-constant sub : integer := 112;
-constant jmp : integer := 128;
-constant jn  : integer := 144;
-constant jp  : integer := 148;
-constant jv  : integer := 152;
-constant jnv : integer := 156;
-constant jz  : integer := 160;
-constant jnz : integer := 164;
-constant jc  : integer := 176;
-constant jnc : integer := 180;
-constant jb  : integer := 184;
-constant jnb : integer := 188;
-constant shr : integer := 224;
-constant shl : integer := 225;
-constant iror: integer := 226;
-constant irol: integer := 227;
-constant hlt : integer := 240;
+architecture rtl of memory is
+
+	constant nop : integer := 0;
+	constant sta : integer := 16;
+	constant lda : integer := 32;
+	constant add : integer := 48;
+	constant ior : integer := 64;
+	constant iand: integer := 80;
+	constant inot: integer := 96;
+	constant sub : integer := 112;
+	constant jmp : integer := 128;
+	constant jn  : integer := 144;
+	constant jp  : integer := 148;
+	constant jv  : integer := 152;
+	constant jnv : integer := 156;
+	constant jz  : integer := 160;
+	constant jnz : integer := 164;
+	constant jc  : integer := 176;
+	constant jnc : integer := 180;
+	constant jb  : integer := 184;
+	constant jnb : integer := 188;
+	constant shr : integer := 224;
+	constant shl : integer := 225;
+	constant iror: integer := 226;
+	constant irol: integer := 227;
+	constant hlt : integer := 240;
+
 type data is array (0 to 255) of integer;
+
 begin
 	process (mem_write,rst)
 		variable data_array: data;
 	begin
 		if (rst='1') then
-			-- contador decrescente de 10 (conteúdo do endereço 130) até 0 
-			data_array(0) := lda;	-- carrega a com (130) (a=10)
+			data_array(0) := lda;
 			data_array(1) := 130;	
-			data_array(2) := sub;	-- subtrai (132) de a (a=a-1)
+			data_array(2) := sub;
 			data_array(3) := 132;
-			data_array(4) := jz;	-- salta para 8 se a=0
+			data_array(4) := jz;	
 			data_array(5) := 8;
-			data_array(6) := jmp;	-- salta para o endereço 2 (loop)
+			data_array(6) := jmp;
 			data_array(7) := 2;
-			-- terminou a contagem, agora faz a soma de (130) com (131) e salva em (128)
-			data_array(8) := lda;	-- carrega a com (130) (a=10)
+			data_array(8) := lda;
 			data_array(9) := 130;
-			data_array(10) := add;	-- soma a com (131) (a=10+18)
+			data_array(10) := add;
 			data_array(11) := 131;
-			data_array(12) := sta;	-- guarda a em (128)
+			data_array(12) := sta;
 			data_array(13) := 128;
-			-- agora faz um or de (128) com (129) rotacionado 4 bits à esquerda, salva o resultado em (133)
-			data_array(14) := lda;	-- carrega a com (129)
+			data_array(14) := lda;	
 			data_array(15) := 129;
-			data_array(16) := shl;	-- desloca a 1 bit à esquerda (o lsb é zero)
-			data_array(17) := shl;	-- desloca a 1 bit à esquerda (o lsb é zero)
-			data_array(18) := shl;	-- desloca a 1 bit à esquerda (o lsb é zero)
-			data_array(19) := shl;	-- desloca a 1 bit à esquerda (o lsb é zero)
-			data_array(20) := ior;	-- ou lógico de a com (128)
+			data_array(16) := shl;
+			data_array(17) := shl;
+			data_array(18) := shl;
+			data_array(19) := shl;
+			data_array(20) := ior;
 			data_array(21) := 128;
-			data_array(22) := sta;	-- guarda o resultado em (133)
+			data_array(22) := sta;
 			data_array(23) := 133;
-			data_array(24) := hlt;	-- pára o processamento
-			-- variáveis e constantes utilizadas no programa
+			data_array(24) := hlt;
+			
 			data_array(128) := 0;
 			data_array(129) := 5;
 			data_array(130) := 10;
@@ -83,4 +83,4 @@ begin
 		end if;
 		data_out <= data_array(address_bus);
 	end process;
-end memo;
+end rtl;
